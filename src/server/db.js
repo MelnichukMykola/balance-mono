07d5f407667ec0bd366abbfb664e0351f5e6db6b
Kolya Melnichuk <nikolfutdeveloper@gmail.com>
2024-06-16 19:03:21 +0200

    0.7 BETA

--- src/server/db.js
+++ src/server/db.js
@@ -1,11 +1,15 @@
 import pg from "pg";
 
 const pool = new pg.Pool({
-  user: "postgres",
-  password: "root",
-  host: "localhost",
-  port: 5433,
-  database: "ballance_auth",
+  user: "balance_mono_db_user",
+  password: "0WzGbzTmqZTuMIAMWGGia6CZ4XpSAhxq",
+  host: "dpg-cpng4sg8fa8c73b0q0a0-a.oregon-postgres.render.com",
+  port: 5432,
+  database: "balance_mono_db",
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
+
