ae79c02db6d373fc0e82214aa951212a41c7642c
Kolya Melnichuk <nikolfutdeveloper@gmail.com>
2024-06-21 08:49:57 +0200

    1.6 BETA

--- src/client/pages/ChartsPage.jsx
+++ src/client/pages/ChartsPage.jsx
@@ -1,18 +1,14 @@
-import { useSelector } from 'react-redux'
 import ContentContainer from '../containers/ContentContainer'
 import FooterContainer from '../containers/FooterContainer'
 import HeaderContainer from '../containers/HeaderContainer'
-import { selectUser } from '../store/selectors'
 import './styles.scss'
 
 function ChartsPage() {
   return (
     <div className='charts-page'>
       <HeaderContainer page='charts' />
-      <div className='overflowed-container'>
-        <ContentContainer chartsOnFullDisplay='true' />
-        <FooterContainer />
-      </div>
+      <ContentContainer chartsOnFullDisplay='true' />
+      <FooterContainer />
     </div>
   )
 }
